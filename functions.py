c55ad26a245a8956a737b47785bc09dce5667a05
mattebur <109103905+mattebur@users.noreply.github.com>
2022-07-11 21:18:07 +0200

    added file with functions to make code better

---
+++ functions.py
@@ -0,0 +1,6 @@
+def printing_board(board):
+    print(board['TL'] + '|' + board['TM'] + '|' + board['TR'])
+    print('------')
+    print(board['ML'] + '|' + board['MM'] + '|' + board['MR'])
+    print('------')
+    print(board['BL'] + '|' + board['BM'] + '|' + board['BR'])
